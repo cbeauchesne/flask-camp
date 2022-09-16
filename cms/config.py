b3bb4c085bf6bc298f1647bde3125356f99d3cb8
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-09-16 15:32:24 +0200

    Config

--- cms/config.py
+++ cms/config.py
@@ -8,16 +8,22 @@ class _BaseConfig:
 class Production(_BaseConfig):
     # DATABASE_URI must be in FLASK_DATABASE_URI env var
     # SECRET_KEY must be set in FLASK_SECRET_KEY env var
-    pass
+
+    RATELIMIT_STORAGE_URL = "memory://"  # TODO
 
 
 class Development(_BaseConfig):
     DATABASE_URI = "sqlite:///sqlite.db"
     SECRET_KEY = "not_very_secret"
 
+    RATELIMIT_STORAGE_URL = "memory://"
+
 
 class Testing(_BaseConfig):
-    DATABASE_URI = "sqlite://"
     TESTING = True
-    RATELIMIT_ENABLED = False
+
+    DATABASE_URI = "sqlite://"
     SECRET_KEY = "not_very_secret"
+
+    RATELIMIT_ENABLED = False
+    RATELIMIT_STORAGE_URL = "memory://"
