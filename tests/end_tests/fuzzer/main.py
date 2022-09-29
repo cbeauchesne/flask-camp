4c96144985673f364a084173fe0e7abca9167abe
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-09-29 19:41:59 +0200

    better fuzzer

--- tests/end_tests/fuzzer/main.py
+++ tests/end_tests/fuzzer/main.py
@@ -1,16 +1,22 @@
+import collections
 import random
 import threading
-
-import requests
+import time
 
 from tests.end_tests.utils import ClientSession
 
 
 class FuzzerSession(ClientSession):
-    known_documents = None
+    def __init__(self):
+        super().__init__()
+        self.known_documents = None
+        self.kill = False
 
     def fuzz_update_known_documents(self):
         self.known_documents = self.get_documents().json()["documents"]
+
+    def fuzz_create_document(self):
+        self.create_document(data=str(random.randbytes(8)), expected_status=200)
 
     def fuzz_modify_document(self):
         if not self.known_documents:
@@ -18,12 +24,12 @@ class FuzzerSession(ClientSession):
 
         doc = random.choice(self.known_documents)
 
-        rc_sleep = random.random() ** 4
+        # rc_sleep = random.random() ** 4
 
         self.modify_document(
             doc,
             data=str(random.randbytes(8)),
-            params={"rc_sleep": rc_sleep},
+            # params={"rc_sleep": rc_sleep},
             expected_status=[200, 409],
         )
 
@@ -47,43 +53,93 @@ class FuzzerSession(ClientSession):
 
         self.get_version(version)
 
+    def fuzz_login(self):
+        i = random.randint(0, 2)
+        self.login_user(f"user_{i}")
+
     def possible_actions(self):
-        return [
-            self.fuzz_update_known_documents,
-            self.fuzz_modify_document,
-            self.fuzz_get_document,
-            self.fuzz_get_version,
+        result = [
+            (self.fuzz_update_known_documents, 30),
+            (self.fuzz_get_document, 50),
+            (self.fuzz_get_version, 5),
         ]
 
+        if self.is_anonymous:
+            result += [(self.fuzz_login, 1)]
+        else:
+            result += [
+                (self.logout_user, 1),
+                (self.fuzz_create_document, 5),
+                (self.fuzz_modify_document, 20),
+            ]
+
+        return [item[0] for item in result], [item[1] for item in result]
+
     def do_some_stuff(self):
-        action = random.choice(self.possible_actions())
+        possible_actions, weights = self.possible_actions()
+        action = random.choices(possible_actions, weights=weights, k=1)[0]
 
         action()
 
+    def run(self):
+        while not self.kill:
+            try:
+                self.do_some_stuff()
+            except:  # pylint: disable=bare-except
+                pass
 
-def run(i):
-    session = FuzzerSession(domain="http://localhost:5000")
-    session.setup_user(f"user_{i}")
 
-    session.create_document()
+def print_stats(sessions):
+    stats = collections.defaultdict(list)
+    for session in sessions:
+        for key, values in session.stats.items():
+            stats[key] += values
 
-    for _ in range(300):
-        try:
-            session.do_some_stuff()
-        except requests.ConnectionError:
-            print("Oooops")
+    sorted_keys = {}
+    for (method, endpoint, outcome), values in stats.items():
+        sort_key = f"{endpoint} {method} {outcome}"
+        sorted_keys[sort_key] = (method, endpoint, outcome)
 
-    print("OK")
+        values.sort()
+
+    for sort_key in sorted(sorted_keys):
+        (method, endpoint, outcome) = sorted_keys[sort_key]
+        values = stats[(method, endpoint, outcome)]
+
+        size = len(values)
+        p_90 = int(values[int(size * 0.9)])
+        p_95 = int(values[int(size * 0.95)])
+        p_99 = int(values[int(size * 0.99)])
+        print(f"{method:6} {endpoint:15} {len(values):5} {p_90:5}ms {p_95:5}ms {p_99:5}ms {outcome}")
+
+    print()
 
 
 def main():
-    threads = []
-    for i in range(10):
-        thread = threading.Thread(target=run, args=(i,))
+
+    sessions = [FuzzerSession() for _ in range(3)]
+
+    for i, session in enumerate(sessions):
+        session.setup_user(f"user_{i}")
+        session.create_document()
+
+    threads = [threading.Thread(target=session.run) for session in sessions]
+
+    for thread in threads:
         thread.start()
+
+    for _ in range(100):
+        time.sleep(1)
+        print_stats(sessions)
+
+    for session in sessions:
+        session.kill = True
 
     for thread in threads:
         thread.join()
+
+    print("#" * 80)
+    print_stats(sessions)
 
 
 if __name__ == "__main__":
--- tests/end_tests/utils.py
+++ tests/end_tests/utils.py
@@ -1,5 +1,4 @@
-from contextlib import contextmanager
-import sys
+import collections
 from time import perf_counter
 
 import requests
@@ -9,13 +8,6 @@ from tests.utils import ClientInterface
 
 
 engine = create_engine("postgresql://cms_user:cms_user@localhost:5432/cms")
-
-
-@contextmanager
-def catch_time():
-    start = perf_counter()
-    yield lambda: total
-    total = perf_counter() - start
 
 
 def get_email_token(user_name):
@@ -28,12 +20,13 @@ def get_email_token(user_name):
 class ClientSession(ClientInterface):
     _session_id = 0
 
-    def __init__(self, domain):
+    def __init__(self, domain="http://localhost:5000"):
         self.domain = domain
         self._session = requests.Session()
         self.logged_user = None
         ClientSession._session_id += 1  # TODO thread lock
         self.session_id = ClientSession._session_id
+        self.stats = collections.defaultdict(list)
 
     @property
     def logged_user_name(self):
@@ -42,21 +35,36 @@ class ClientSession(ClientInterface):
     def __str__(self):
         return f"S#{self.session_id} ({self.logged_user_name})"
 
-    def _request(self, method, url, expected_status=None, **kwargs):
-        print(f"{str(self):20} {method.upper():6} {url:30}")
-        with catch_time() as t:
-            r = self._session.request(method, f"{self.domain}{url}", **kwargs, timeout=10)
+    def add_stat(self, method, url, outcome, ellapsed):
+        key = (method.upper(), url.split("/")[1], outcome)
+        self.stats[key].append(ellapsed)
 
-        print(f"{str(self):20} {method.upper():6} {url:30} {r.status_code} ({int(t()*1000)}ms)")
+    def _request(self, method, url, expected_status=None, **kwargs):
+        outcome = None
+
+        start = perf_counter()
+        try:
+            r = self._session.request(method, f"{self.domain}{url}", **kwargs, timeout=10)
+            outcome = str(r.status_code)
+        except Exception as e:
+            outcome = str(e)
+            raise
+        finally:
+            ellapsed = (perf_counter() - start) * 1000
+            self.add_stat(method, url, outcome, ellapsed)
 
         expected_status = 200 if expected_status is None else expected_status
 
         expected_status = expected_status if isinstance(expected_status, (list, tuple, set)) else [expected_status]
 
         if r.status_code not in expected_status:
-            print(f"\tExpected status is: {expected_status}")
-            print(f"\tContent is: {r.content}")
-            sys.exit(1)
+            print(f"{method.upper()} {url}")
+            print(f"Params: {kwargs.get('params')}")
+            print(f"Expected status: {expected_status}")
+            print(f"Observed status: {r.status_code}")
+            print(f"Headers: {r.headers}")
+            print(f"Content: {r.content}")
+            raise ValueError()
 
         return r
 
@@ -82,7 +90,19 @@ class ClientSession(ClientInterface):
 
         return r
 
+    def logout_user(self, expected_status=None):
+        r = super().logout_user(expected_status=expected_status)
+
+        if r.status_code == 200:
+            self.logged_user = None
+
+        return r
+
     def setup_user(self, name):
         self.create_user(name)
         self.validate_email(name, get_email_token(name))
         self.login_user(name)
+
+    @property
+    def is_anonymous(self):
+        return self.logged_user is None
